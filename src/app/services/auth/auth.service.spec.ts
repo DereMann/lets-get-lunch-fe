e23fc31aa14fb0441626fae22915f272961b14be
DereMann <martinstimpfl@gmx.at>
2019-02-04 22:12:35 +0000

    test

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -2,18 +2,31 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 
-describe('AuthService', () => {
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+
+fdescribe('AuthService', () => {
   let authService: AuthService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [AuthService]
     });
-    
+
     authService = TestBed.get(AuthService);
+    http = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(authService).toBeTruthy();
+  });
+
+  describe('signup', () => {
+    it('should return a user object with a valid username and password', () => {
+    });
   });
 });
